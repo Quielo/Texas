e80aa990a391f462be8497e1eb2d69f3650189c2
Quielo <mty.gonzalez@gmail.com>
2021-06-21 15:15:36 -0500

    Server con Cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.use(express.static('public'));
+app.use(cors());
 
 const { Deck, Hand } = require('./public/deck');
 
